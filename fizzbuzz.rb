2f15dfc5110a01d5f1dbaed4e86acfc74378125d
mitsumakiNT <mitumakifr@gmail.com>
2020-06-26 01:02:14 +0900

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,3 +12,22 @@ for i in 1..int
       puts "#{i}"
   end
 end
+
+# another version
+
+int = gets.to_i
+
+(1..int).each do |i|
+
+
+    if i % 15 == 0
+        puts "Fizz Buzz"
+    elsif i % 3 == 0
+        puts "Fizz"
+    elsif i % 5 == 0 
+        puts "Buzz"
+    else
+        puts i
+    end
+
+end
