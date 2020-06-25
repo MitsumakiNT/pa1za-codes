78af74c4161759cb3e8eddcb82eef31af96552a5
mitsumakiNT <mitumakifr@gmail.com>
2020-06-25 13:00:21 +0900

    Create count_matching_string_of_array.rb

---
+++ count_matching_string_of_array.rb
@@ -0,0 +1,6 @@
+input = gets.chomp.split("") #与えられた文字列の要素配列
+p input
+num = input.length #与えられた文字列の長さを出力し、ques内の配列をいくつ取り出すか決定する
+ques = gets.chomp.split("") #判別すべき文字列の要素配列
+# p ques	
+# ["a", "b", "d", "e", "e", "A", "A", "b", "A", "A", "A", "b", "f", "d", "e"]
