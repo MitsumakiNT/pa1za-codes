20ebeca79fe32505042ff9bbd2ff70ed3d8d1cce
mitsumakiNT <mitumakifr@gmail.com>
2020-06-25 10:23:37 +0900

    Create Sort by Strings and integers.rb

---
+++ Sort by Strings and integers.rb
@@ -0,0 +1,14 @@
+num = gets.chomp.to_i
+
+array = {}
+
+(1..num).each do
+  line = gets.chomp.split(' ')
+  array[line[1].to_i] = line[0]
+end
+
+array = array.sort
+
+array.each do |ele|
+  puts ele[1]
+end
