c3e418c18161a3d5e609dbc317dfb341c820933c
mitsumakiNT <mitumakifr@gmail.com>
2020-06-25 09:53:40 +0900

    Create add or multiply.rb

---
+++ add or multiply.rb
@@ -0,0 +1,15 @@
+times = gets.to_i
+ans = 0
+for i in 1..times
+  line = gets.split(" ").map(&:to_i)
+  a = line[0]
+  b = line[1]
+  
+  if a == b
+      input = a * b
+  else
+      input = a + b
+  end
+  ans = ans + input
+end
+ p ans
