1ad946bd2fe51dd3907a594b1af057c05f5a910a
mitsumakiNT <mitumakifr@gmail.com>
2020-06-25 09:56:40 +0900

    Create Capitalize letters.rb

---
+++ Capitalize letters.rb
@@ -0,0 +1,19 @@
+input = gets.split(" ").map(&:to_i)
+
+a = input[0]
+b = input[1]
+
+str = gets.chomp.split("")
+# p str
+# ["t", "h", "i", "s", " ", "i", "s", " ", "a", " ", "p", "e", "n"]
+str.each_with_index do |s,t|
+     #文字列の客観的なインデックス番号がt
+    if t >= (a - 1) && t <= (b - 1)
+        print s.upcase
+    else 
+        print s
+    end
+    # print s
+    # print t
+    # puts ""
+end
