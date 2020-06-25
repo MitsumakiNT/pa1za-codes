db60f6fddb3cdf59731d3f78716d3d2b6bdd6a23
mitsumakiNT <mitumakifr@gmail.com>
2020-06-25 10:44:46 +0900

    Create n_n+1.rb

---
+++ n_n+1.rb
@@ -0,0 +1,18 @@
+num = gets.to_i
+word = gets.chomp.split("")
+
+a = word[num - 1]
+b = word[num]
+
+if num == word.length
+    print ""
+else
+    print "#{a} #{b}"
+end
+
+# another version ↓
+
+n = gets.chomp.to_i
+str = gets.chomp
+
+puts "#{str[n - 1]} #{str[n]}" if str[n]
