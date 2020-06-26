b3fa0b8e6162f224f2d80f2c6a430fbc907a21b0
mitsumakiNT <mitumakifr@gmail.com>
2020-06-26 09:44:48 +0900

    Update lucky_number.rb

--- lucky_number.rb
+++ lucky_number.rb
@@ -11,13 +11,13 @@ for i in 1..num
   buy_second = buy % 10000 #当選番号しも４桁
   buy_third = buy % 1000 #当選番号しも３桁
   if buy == lucky
-      puts "first"
+      puts "first" #一等
       elsif buy == lucky + 1 || buy == lucky - 1
-      puts "adjacent"
+      puts "adjacent" #前後賞
       elsif buy_second == second
-      puts "second"
+      puts "second" #二等
       elsif buy_third == third
-      puts "third"
+      puts "third" #三等
   else 
       puts "blank"
   end
