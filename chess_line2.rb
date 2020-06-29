d527c2b8714d02a8e449aa2e03fce65f5eccc8ab
mitsumakiNT <mitumakifr@gmail.com>
2020-06-29 17:30:27 +0900

    Create chess_line2.rb

---
+++ chess_line2.rb
@@ -0,0 +1,21 @@
+winner1 = ["O","O","O","O","O"]
+winner2 = ["X","X","X","X","X"]
+winner3 = [".",".",".",".","."]
+a = 0
+for i in 1..5
+  input = gets.chomp.split("")
+  if input == winner1 
+      puts "O"
+      elsif input == winner2
+      puts "X"
+      elsif input == winner3
+      puts "."
+  else
+      a = a + 1
+  end
+end
+
+if a == 5
+    puts "D"
+end
+    
