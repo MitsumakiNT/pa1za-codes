7dc5c8d9f39fe9fa03f510068f3ea84bc3772d93
mitsumakiNT <mitumakifr@gmail.com>
2020-07-13 08:50:15 +0900

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,16 @@
 int = gets.to_i
 
 for i in 1..int
-  if i % 15 == 0
-      puts "Fizz Buzz"       
-  elsif i % 3 == 0
-      puts "Fizz"
-        
-  elsif i % 5 == 0
-      puts "Buzz"
-  else 
-      puts "#{i}"
-  end
+    if i % 15 == 0
+        puts "Fizz Buzz"       
+    elsif i % 3 == 0
+        puts "Fizz"
+            
+    elsif i % 5 == 0
+        puts "Buzz"
+    else 
+        puts "#{i}"
+    end
 end
 
 # another version
