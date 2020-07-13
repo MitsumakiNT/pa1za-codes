c5a6097963ed8d0613a561afde5a021bd2890053
mitsumakiNT <mitumakifr@gmail.com>
2020-07-13 10:29:38 +0900

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -35,5 +35,11 @@ end
 while i <= 30
     if i % 15 == 0
         puts "FizzBuzz"
+    elsif i % 5 == 0
+        puts "Buzz"
+    elsif i % 3 == 0
+        puts "Fizz"
+    else
+        puts i
     end
 end
