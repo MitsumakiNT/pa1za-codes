c8efe97afe68ac685a7d1f364417665c3faf6293
mitsumakiNT <mitumakifr@gmail.com>
2020-07-16 18:11:23 +0900

    Create function.js

---
+++ function.js
@@ -0,0 +1,27 @@
+var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+$(function(){
+  let list = $("#list");
+
+  function appendList(word){
+    let item = $('<li class = "list>').append(word);
+    list.append(item);
+  }
+
+  $("#submit").on("click", function(){
+    let input = $("#keyword").val();
+    let reg = new RegExp("^" + input);
+
+    $(".list").remove();
+
+    $.each(fruits, function(i, fruit){
+      if (fruit.match(reg)){
+        appendList(fruit);
+      }
+    });
+
+    if ($(".list").length === 0){
+      appendList("一致する果物はありませんでした");
+    }
+  });
+});
