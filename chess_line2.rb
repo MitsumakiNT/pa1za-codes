d210eede71a80cdcdd1d627ed59297063006b536
mitsumakiNT <mitumakifr@gmail.com>
2020-07-27 17:30:20 +0900

    Update chess_line2.rb

--- chess_line2.rb
+++ chess_line2.rb
@@ -5,14 +5,14 @@ a = 0
 for i in 1..5
   input = gets.chomp.split("")
   if input == winner1 
-      puts "O"
-      elsif input == winner2
-      puts "X"
-      elsif input == winner3
-      puts "."
-  else
-      a = a + 1
-  end
+    puts "O"
+    elsif input == winner2
+    puts "X"
+    elsif input == winner3
+    puts "."
+    else
+    a = a + 1
+    end
 end
 
 if a == 5
