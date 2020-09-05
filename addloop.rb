3bda887b8d827fb7fe5c7d1a4a2e1f049d5d6ac5
mitsumakiNT <mitumakifr@gmail.com>
2020-09-05 14:19:06 +0900

    Update addloop.rb

--- addloop.rb
+++ addloop.rb
@@ -5,4 +5,9 @@ end
 (1..5).map{|i| i*i }              # —— (1)と同じ
 
 %w(a b c).map{|char| char.upcase} # —— (2)
-%w(a b c).map(&:upcase)           # —— (2)と同
+%w(a b c).map(&:upcase)           # —— (2)と同じ
+
+params = {}
+params[:user] ={ name: "Onsen Ruby", email: "onsenruby@onsen.com" } 
+p params 
+# => {:user=>{:name=>"Onsen Ruby", :email=>"onsenruby@onsen.com"}}
