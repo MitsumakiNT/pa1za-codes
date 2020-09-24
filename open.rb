236edca9d939febed6d1c0cac81e11dd863437c4
mitsumakiNT <mitumakifr@gmail.com>
2020-09-24 15:39:14 +0900

    Create open.rb

---
+++ open.rb
@@ -0,0 +1,12 @@
+words = []
+
+puts '好きな数だけ単語を入力してください'
+puts '最後はEnterだけの空行にしてください'
+
+while word = gets.chomp
+    break if word.empty?
+    words << word
+end
+
+puts '単語をアルファベット順に並べます'
+puts words.sort
