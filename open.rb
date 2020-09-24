f49869a75cb3de64d4feae0ced6f4166d98a8b2d
mitsumakiNT <mitumakifr@gmail.com>
2020-09-24 15:39:36 +0900

    Update open.rb

--- open.rb
+++ open.rb
@@ -10,3 +10,19 @@ end
 
 puts '単語をアルファベット順に並べます'
 puts words.sort
+
+##or
+
+words = []
+word =
+
+puts '好きな数だけ単語を入力してください'
+puts '最後はEnterだけの空行にしてください'
+
+while word != ""
+  word = gets.chomp
+  word << newWord
+end
+
+puts '単語をアルファベット順に並べます'
+puts words.sort
