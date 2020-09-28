22279e3e000e3969caab06a732d40c0ad4f28c5f
mitsumakiNT <mitumakifr@gmail.com>
2020-09-28 15:11:26 +0900

    Update multitask.rb

--- multitask.rb
+++ multitask.rb
@@ -34,3 +34,18 @@ success_str = 'ペンパイナッポーアッポーペン'
 pathfinder(word_ary, correct_ary, success_str)
 
 #イニシャライズメソッドを書くべきか？
+
+def likes(names)
+  return "no one likes this" if names.empty?
+
+  if names.count == 1
+    "#{names[0]} likes this"
+  elsif names.count == 2
+    names.each { |n| }.join(" and ") << " like this"
+  elsif names.count == 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+　　　others_count = names.count -2
+    "#{names[0]}, #{names[1]} and #{others_count} others like this"   
+  end
+end
