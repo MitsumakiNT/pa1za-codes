fd38b548795fcd53235d7af45655a35eddfc1117
mitsumakiNT <mitumakifr@gmail.com>
2020-10-04 14:39:31 +0900

    Update addloop.rb

--- addloop.rb
+++ addloop.rb
@@ -28,3 +28,45 @@ count = 1
 
 print("¥n")
 
+# for 文
+# 奇数であれば後続の処理( puts )をスキップします。
+for i in [1,2,3,4,5]
+  next if i.odd?
+  puts i
+end
+# while 文で書いてみよう。
+# 奇数であれば後続の処理( puts )をスキップします。
+j = 0
+while j < 5 do
+  j = j + 1
+  next if j.odd?
+  puts j
+end
+# until 文で書いてみよう。
+# 奇数であれば後続の処理( puts )をスキップします。
+k = 0
+until k >= 5 do
+  k = k + 1
+  next if k.odd?
+  puts k
+end
+# loop 文で書いてみよう。
+# 奇数であれば後続の処理( puts )をスキップします。
+l = 0
+loop do
+  break if l >= 5
+  l = l + 1
+  next if l.odd?
+  puts l
+end
+# next は一番内側のループしかスキップしない点を意識してください。
+for v in ["a","b","c"]
+  i = 0
+  until i >= 5 do 
+    i+=1
+    next if i.odd?
+    puts i
+  end
+  puts v
+end
+__END__
