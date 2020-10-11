b84ab84d080a25bea541bdccc4c3de924becd92f
mitsumakiNT <mitumakifr@gmail.com>
2020-10-11 18:26:30 +0900

    Update judge.rb

--- judge.rb
+++ judge.rb
@@ -15,3 +15,16 @@ ary = [1, 3, 5, 6, 8]
 
 # 2〜5のいずれかが含まれる
 ary.any? {|i| i.between?(2, 5)}
+
+module Include
+  refine Array do
+    # all: trueの場合
+    #   listの値全てがselfに含まれているか判定
+    # all: falseの場合
+    #   listの値のいずれかがselfに含まれているか判定
+    def include?(*list, all: true)
+      method = all ? :all? : :any?
+      list.__send__(method) {|i| super(i)}
+    end
+  end
+end
