b77a13c1742d9b2b179d8a118a07a0fc3e885fbb
mitsumakiNT <mitumakifr@gmail.com>
2020-10-18 14:17:17 +0900

    Update osarai.rb

--- osarai.rb
+++ osarai.rb
@@ -75,3 +75,18 @@ for i in 1..num
 end
 
 puts total_sum
+
+
+
+num = gets.chomp.to_i
+
+user2blood = {}
+
+(1..num).each do
+  array = gets.chomp.split(' ')
+  user2blood[array[0]] = array[1]
+end
+
+user2blood.each do |key, value|
+  puts "#{key} #{value}"
+end
