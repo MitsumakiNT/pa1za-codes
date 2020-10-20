7c27436f6754bc820ee386cb6f6a1f6a0ecc9e71
mitsumakiNT <mitumakifr@gmail.com>
2020-10-20 17:35:41 +0900

    Update Bset.rb

--- Bset.rb
+++ Bset.rb
@@ -1,3 +1,7 @@
 #value参照でソート
 math = {"あだち" => 40, "いいだ" => 12, "えんどう" => 99}
 math.sort_by{|k,v| v }
+
+#標準入力別解
+lines = readlines.map{|line| line.split(' ')}
+lines = readlines.map{|line| line.split(' ').map(&:to_i)}
