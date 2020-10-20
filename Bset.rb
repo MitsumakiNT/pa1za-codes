f91b75f7e9bf0c4a8d192412b4dbddf95df471a4
mitsumakiNT <mitumakifr@gmail.com>
2020-10-20 19:12:23 +0900

    Update Bset.rb

--- Bset.rb
+++ Bset.rb
@@ -5,3 +5,20 @@ math.sort_by{|k,v| v }
 #標準入力別解
 lines = readlines.map{|line| line.split(' ')}
 lines = readlines.map{|line| line.split(' ').map(&:to_i)}
+
+#例題、お一人様一回限り
+times = gets.to_i
+names = []
+for i in 1..times
+  name = gets.chomp
+  names.push(name)
+  counter = names.group_by(&:itself).map{ |k, v| [k, v.count] }.to_h
+#   p counter
+#   {"James"=>1, "Mary"=>1, "John"=>1}
+  if counter[name] == 1
+      puts "YES"
+  else 
+      counter[name] - 1
+      puts "NO"
+  end
+end
