6ebc9d6228dea614f1fdc5bc0ab60c23a94119bc
mitsumakiNT <mitumakifr@gmail.com>
2020-10-22 11:44:34 +0900

    Update hairetsukatuyou.rb

--- hairetsukatuyou.rb
+++ hairetsukatuyou.rb
@@ -51,3 +51,13 @@ for i in 0..4
       puts ans
   end
 end
+
+#配列作成簡略化
+numbers = [12, 34, 56, 78, 90]
+
+# ここに、各要素を3倍にして新しい配列を作成するコードを記述する
+numbers2 = numbers.map do |num |
+    num * 3
+end
+
+p numbers2
